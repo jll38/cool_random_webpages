5c0941345bc5f1718bd3af7891f10ea88845261b
Julian <jll38@njit.edu>
2022-09-15 12:36:35 -0400

    404 Page

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Cool Little Portfolio Website Thing
 I'm making this website as like a little hub for some coding experiments and practice.
+
+https://cool-stuff-and-projects.herokuapp.com/
--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 from simple_pages import simple_pages
 from flask_bootstrap import Bootstrap
 
+def page_not_found(e):
+    return render_template("404.html"), 404
 app = Flask(__name__)
 
 app.register_blueprint(simple_pages)
+app.register_error_handler(404, page_not_found)
 
 if __name__ == '__main__':
     app.run()
