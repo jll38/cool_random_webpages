4855c39fe2396283f2ada356d00695d293d6631a
jll38 <jll38@njit.edu>
2022-09-14 23:21:04 -0400

    Gunicorn Fix

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from simple_pages import simple_pages
 from flask_bootstrap import Bootstrap
+def create_app():
+    app = Flask(__name__)
 
-app = Flask(__name__)
+    app.register_blueprint(simple_pages)
 
-app.register_blueprint(simple_pages)
+    return app
 
-if __name__ == '__main__':
-    app.run()
+
+
+
