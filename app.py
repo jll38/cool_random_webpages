1d5add352f7c7ff4896ead62d683f5ca7ffed799
jll38 <jll38@njit.edu>
2022-09-14 21:35:50 -0400

    Initial Commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from simple_pages import simple_pages
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+
+app.register_blueprint(simple_pages)
+
+if __name__ == '__main__':
+    app.run()
---
+++ simple_pages/__init__.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, render_template,abort
+from jinja2 import TemplateNotFound
+
+simple_pages = Blueprint('simple_pages', __name__,
+                         template_folder='templates')
+@simple_pages.route('/')
+def home():
+    try:
+        return render_template('index.html')
+    except TemplateNotFound:
+        abort(404)
+
+@simple_pages.route('/NumGenerator')
+def randNum():
+    try:
+        return render_template('rand.html')
+    except TemplateNotFound:
+        abort(404)
