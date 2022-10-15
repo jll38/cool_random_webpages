d0ca136b1e60439b535defb07ea31b2596db27b2
jll38 <jll38@njit.edu>
2022-10-14 20:48:24 -0400

    pytube testing

--- simple_pages/__init__.py
+++ simple_pages/__init__.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template,abort
+from flask import Blueprint, render_template,abort, request
 from jinja2 import TemplateNotFound
+import pytube
 
 simple_pages = Blueprint('simple_pages', __name__,
                          template_folder='templates')
@@ -28,5 +29,18 @@ def pigLatin():
 def calc():
     try:
         return render_template('calc.html')
+    except TemplateNotFound:
+        abort(404)
+
+@simple_pages.route('/youtube', methods =["GET", "POST"])
+def ytDownloader():
+    if request.method =="POST":
+        link = str(request.form.get("link"))
+        yt=pytube.YouTube(link)
+        stream = yt.streams.get_audio_only()
+        stream.download()
+
+    try:
+        return render_template('yt_down.html')
     except TemplateNotFound:
         abort(404)
